ab286b3ecc5f4bc38ad0d3305832eb9a11b227a2
Mithi Sevilla <mithi.sevilla@gmail.com>
2017-07-30 05:10:52 +0800

    feat: Add get nearest gap function

--- headers/nearby.h
+++ headers/nearby.h
@@ -1,6 +1,28 @@
+#include <vector>
 #include "helper.h"
 #include "vehicle.h"
-#include <vector>
+
+/* Get gap between nearest vehicle from either our front or back
+   at a given given lane
+  NOTE: where: FROM_FRONT = 1.0, FROM_BACK = -1
+ */
+double get_nearest_gap(
+  const Vehicle &myCar, const std::vector<Vehicle>& otherCars, const LaneType lane_type, double where){
+
+  double smallest_gap = REALLY_BIG_NUMBER;
+
+  for(auto &otherCar: otherCars){
+
+    double gap = (otherCar.s - myCar.s) * where;
+
+    if (otherCar.lane == lane_type && gap > 0.0 && gap < smallest_gap) {
+      smallest_gap = gap;
+    }
+  }
+
+  return smallest_gap;
+}
+
 
 /* NearbyVehicleInfo - stores a vehicle object (vehicle),
  * how far away it is from us (gap) in road distance
@@ -12,8 +34,10 @@ struct NearbyVehicleInfo {
 };
 
 /*
-  NOTE: These two functions below could clearly use some refactoring and/or
-        performance optimization IMHO
+  NOTE: - These two functions below could clearly use some refactoring and/or
+          performance optimization IMHO.
+        - Also currently, the computation does not take into account that this
+          is a highway loop. We should probably use modulo for this
  */
 NearbyVehicleInfo get_nearest_front(
   const Vehicle &myCar, const std::vector<Vehicle>& otherCars, const LaneType lane_type){
