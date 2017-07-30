49fd49c6e07a69cf08d246cae1dc00d942c7ccb4
Mithi Sevilla <mithi.sevilla@gmail.com>
2017-07-31 04:51:03 +0800

    style: Tweak parameters

--- headers/helper.h
+++ headers/helper.h
@@ -11,17 +11,17 @@ const double REALLY_BIG_NUMBER = 1000000.0;
 const double TRACK_DISTANCE = 6945.554;
 
 const double HARD_SPEED_LIMIT = 22.352; // 50mph in m/s
-const double SPEED_LIMIT = 22.0;
+const double SPEED_LIMIT = 21.0;
 
 const double TIME_INCREMENT = 0.02;
-const double TRAVERSE_TIME = 2.5;
+const double TRAVERSE_TIME = 2.0;
 const int NUMBER_OF_POINTS = int(TRAVERSE_TIME / TIME_INCREMENT);
 
 const double FRONT_GAP_THRESH = 30.0; // 0, 5, 10, 15, 20
 const double BACK_GAP_THRESH = 20.0; // 0, 5, 10
 const double CHANGE_LANE_PENALTY = 0.0; // 0, 5
 
-const int  PATH_SIZE_CUTOFF = 15; // 10, 15, 20
+const int  PATH_SIZE_CUTOFF = 20; // 10, 15, 20
 
 enum class LaneType {
   LEFT, MID, RIGHT, NONE, UNSPECIFIED
