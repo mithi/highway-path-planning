0f5814902a6cd18972ced5708b0f32730355ec23
Mithi Sevilla <mithi.sevilla@gmail.com>
2017-07-31 04:30:05 +0800

    Feat: Add trajectory class

---
+++ headers/trajectory.h
@@ -0,0 +1,24 @@
+#ifndef TRAJECTORY_H_
+#define TRAJECTORY_H_
+
+#include <vector>
+#include "helper.h"
+#include "vehicle.h"
+#include "jmt.h"
+#include "trajectory.h"
+
+class Trajectory {
+
+  public:
+    Trajectory(Vehicle& car, const BehaviorType behavior);
+    State targetState_d;
+    State targetState_s;
+    JMT get_jmt_s() const;
+    JMT get_jmt_d() const ;
+
+  private:
+    std::vector<JMT> jmtPair;
+
+};
+
+#endif //TRAJECTORY_H_
---
+++ src/trajectory.cpp
@@ -0,0 +1,46 @@
+#include "trajectory.h"
+
+Trajectory::Trajectory(Vehicle& car, const BehaviorType behavior){
+
+  // get target states based on behavior s component
+  double target_s = car.saved_state_s.p + TRAVERSE_TIME * car.saved_state_s.v;
+  double target_v = car.saved_state_s.v;
+
+  if (behavior == BehaviorType::KEEPLANE) {
+
+    bool safe = (car.front_v > HARD_SPEED_LIMIT) || (car.front_gap > FRONT_GAP_THRESH);
+    target_v =  safe ? SPEED_LIMIT : car.front_v;
+    target_s =  car.saved_state_s.p + TRAVERSE_TIME * 0.5 * (car.saved_state_s.v + target_v);
+  }
+
+  if (target_s > TRACK_DISTANCE) {
+    target_s = TRACK_DISTANCE;
+  }
+
+  this->targetState_s = {target_s, target_v, 0.0};
+
+  // get target states based on behavior d component
+  double target_d = car.convert_lane_to_d();
+
+  if (behavior == BehaviorType::TURNLEFT) {
+    target_d = car.convert_lane_to_d(car.lane_at_left);
+  } else if (behavior == BehaviorType::TURNRIGHT) {
+    target_d = car.convert_lane_to_d(car.lane_at_right);
+  }
+
+  this->targetState_d = {target_d, 0.0, 0.0};
+
+  // generate JMTs
+  JMT jmt_s(car.saved_state_s, targetState_s, TRAVERSE_TIME);
+  JMT jmt_d(car.saved_state_d, targetState_d, TRAVERSE_TIME);
+  this->jmtPair.emplace_back(jmt_s);
+  this->jmtPair.emplace_back(jmt_d);
+}
+
+JMT Trajectory::get_jmt_s() const {
+  return jmtPair[0];
+}
+
+JMT Trajectory::get_jmt_d() const {
+  return jmtPair[1];
+}
