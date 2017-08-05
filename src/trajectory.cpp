c9e03f9cbc6cdd464c4981a1ce34f11343768690
Mithi Sevilla <mithi.sevilla@gmail.com>
2017-08-05 10:50:25 +0800

    style: Add comments

--- src/trajectory.cpp
+++ src/trajectory.cpp
@@ -8,15 +8,23 @@ Trajectory::Trajectory(Vehicle& car, const BehaviorType behavior){
 
   if (behavior == BehaviorType::KEEPLANE) {
 
+    // If the car in front is going fast or we are very far from it anyway, go as fast as we can
+    // Else let's go a notch slower than the car in front
     bool safe = (car.front_v > SPEED_LIMIT) || (car.front_gap > FRONT_BUFFER);
     target_v =  safe ? SPEED_LIMIT : (car.front_v - SPEED_BUFFER);
+    
+    // But if the car in front is too slow, let's go a little faster
     target_v = target_v > MIN_SPEED ? target_v : MIN_SPEED;
+    
+    // Estimate a safe target distance based on our selected speed
     target_s =  car.saved_state_s.p + TRAVERSE_TIME * 0.5 * (car.saved_state_s.v + target_v);
   }
-
+  
+  // target acceleration along the load is zero
   this->targetState_s = {target_s, target_v, 0.0};
 
-  // get target states based on behavior d component
+  // get target d component state based on behavior
+  // target speed and acceleration sideways of the road are both zero
   this->targetState_d = {car.get_target_d(behavior), 0.0, 0.0};
 
   // generate JMTs
